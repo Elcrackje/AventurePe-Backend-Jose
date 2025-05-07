31e749aba8f244f9c8bf6d9bc32bd371f6dcbb2e
Jose Gutierrez <josecitogutigar21@gmail.com>
2025-05-07 01:44:10 -0500

    fix(deploy): fix deployment configuration

--- src/main/java/com/upc/aventurape/platform/shared/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/upc/aventurape/platform/shared/infrastructure/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.upc.aventurape.platform.shared.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +12,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // deshabilita CSRF
+                .csrf(AbstractHttpConfigurer::disable) // deshabilita CSRF
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",                        // tu redirección
