113cfeaa7b686fe14efce97455822892f5220fe9
Jose Gutierrez <josecitogutigar21@gmail.com>
2025-05-07 01:56:45 -0500

    feat(deploy): add settings for deplyoment

---
+++ src/main/java/com/upc/aventurape/platform/shared/infrastructure/controllers/PingController.java
@@ -0,0 +1,12 @@
+package com.upc.aventurape.platform.shared.infrastructure.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
